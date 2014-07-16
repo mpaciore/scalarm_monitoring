b4320ccc83ebc67b1148f83192ed55f937671e54
yuri-krylov <michalwrona4@gmail.com>
2014-07-16 21:01:30 +0200

    Recent changes

--- src/manager/parsers/is_parser.go
+++ src/manager/parsers/is_parser.go
@@ -7,13 +7,18 @@ import (
 )
 
 type IS_data struct {
-	Exp_man_address string		
+	Exp_man_address []string		
 }
 
 
 func Get_is_data_encode(json_data []byte) (*IS_data, error){
 	var t IS_data
-	err := json.Unmarshal(json_data, &t)
+	
+	new_json_data := []byte("{ \"Exp_man_address\":")
+	new_json_data = append(new_json_data, json_data...)
+	new_json_data = append(new_json_data, []byte("}")...)
+
+	err := json.Unmarshal(new_json_data, &t)
 	utils.Check(err)
 	return &t, nil// do przemyslenia czy nie da sie lepiej
 }
