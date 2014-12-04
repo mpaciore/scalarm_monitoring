b26f2f84660b347caaa8cb22c2b45af5dbf56715
kliput <jakub.liput@gmail.com>
2014-12-04 14:57:41 +0100

    Some spaces added

--- scalarmMonitoring.go
+++ scalarmMonitoring.go
@@ -37,7 +37,7 @@ func main() {
 	log.Printf("\tpassword:                    %v", configData.Password)
 	log.Printf("\tinfrastructures:             %v", configData.Infrastructures)
 	log.Printf("\tScalarm certificate path:    %v", configData.ScalarmCertificatePath)
-	log.Printf("\tInsecure SSL:       %v", configData.InsecureSSL)
+	log.Printf("\tinsecure SSL:                %v", configData.InsecureSSL)
 	log.Printf("\tScalarm scheme:              %v", configData.ScalarmScheme)
 
 	//create EM connector
